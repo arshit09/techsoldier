d420be64e633a18036b7c98a06017273f10f1ca7
arshit09 <arshitvaghasiya29@gmail.com>
2019-08-07 03:50:48 +0530

    db entry

--- Login/index.php
+++ Login/index.php
@@ -29,23 +29,22 @@
 <link href="https://fonts.googleapis.com/css?family=Roboto&display=swap" rel="stylesheet" href="css/main.css">
 </head>
 <body>
-	
 	<div class="limiter">
 		<div class="container-login100">
 			<div class="wrap-login100 p-b-160 p-t-50">
-				<form class="login100-form validate-form">
+				<form action="process.php" class="login100-form validate-form" method="POST">
 					<span class="login100-form-title p-b-43">
 						Login
 					</span>
 					
 					<div class="wrap-input100 rs1 validate-input" data-validate = "Username is required">
-						<input class="input100" type="text" name="username" >
+						<input class="input100" type="text" name="user" id="user" >
 						<span class="label-input100">Username</span>
 					</div>
 					
 					
 					<div class="wrap-input100 rs2 validate-input" data-validate="Password is required">
-						<input class="input100" type="password" name="pass">
+						<input class="input100" type="password" name="pass" id="pass">
 						<span class="label-input100">Password</span>
 					</div>
 
